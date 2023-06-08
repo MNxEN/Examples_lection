7d36527dca36371228a7e276e3c97bd6c7c80e91
Mikhail Chekushkin <rbmihey@mail.ru>
2023-06-09 01:59:38 +0500

    Add Example005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Whats you name?");
+string username = Console.ReadLine();
+string likename = "Михаил";
+if (username == likename) 
+{
+   Console.Write("О, "); 
+   Console.Write(username);
+   Console.Write(", как я рад тебя видеть!");
+} 
+else
+{
+    Console.Write("Ну привет, "); 
+    Console.Write(username);   
+}
+//Console.WriteLine("Hello, World!");
